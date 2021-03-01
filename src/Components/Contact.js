055600ab8d89461ea8032d95e8da5eeac86a3e26
number 1 dev <Lukemwen@hotmail.com>
2021-03-01 12:57:24 +0000

    Update 1.0.0

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,11 +2,69 @@ import React from "react";
 import { Row, Col } from "react-bootstrap";
 import '../Styles/dist/Contact.css';
 
-class Contact extends React.Component{
-    render(){
+function Contact(){
+    const mql = window.matchMedia('(max-width: 600px)');
+    let phoneView = mql.matches;
+    if(phoneView){
+        return(
+            <div className="phone-contact-container">
+                <Row>
+                    <Col style={{textAlign: 'left', paddingLeft: '50px'}}>
+                        <h4>✦ lMwenda</h4>
+                    </Col>
+                    <Col style={{textAlign: 'right', paddingRight: '90px',  
+                    paddingBottom: '10px', width: '300px'}}>
+                        <h5>Contacts & Resources</h5>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col style={{color: '#fff', paddingLeft: '250px', paddingBottom: '10px'}}>
+                        <a style={{color: '#fff'}} href="https://github.com/lmwenda">
+                            Github Profile
+                        </a>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col style={{color: '#fff', paddingLeft: '325px', marginTop: '10px'}}>
+                        Lukemwen@hotmail.com
+                    </Col>
+                    <Col style={{color: '#fff', paddingLeft: '305px', marginTop: '10px'}}>
+                        Copyright &copy; lMwenda
+                    </Col>
+                </Row>
+            </div>
+        );
+    } else{
         return(
             <div className="contact-container">
-                
+                <Row>
+                    <Col style={{textAlign: 'left', paddingLeft: '50px'}}>
+                        <h4>✦ lMwenda</h4>
+                    </Col>
+                    <Col style={{textAlign: 'right', paddingRight: '155px',  
+                    paddingBottom: '10px'}}>
+                        <h5>Contacts and Resources</h5>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col style={{color: '#fff', paddingLeft: '875px'}}>
+                        Discord Profile: number 1 dev#1019
+                    </Col>
+                </Row>
+                <Row>
+                    <Col style={{color: '#fff', paddingLeft: '800px', marginTop: '10px'}}>
+                        Lukemwen@hotmail.com
+                    </Col>
+                </Row>
+                <Row>
+                    <Col style={{color: '#fff', textAlign: 'right', paddingRight: '290px',
+                    paddingTop: '10px'
+                    }}>
+                        <a style={{color: '#fff'}} href="https://github.com/lmwenda">
+                            Github Profile
+                        </a>
+                    </Col>
+                </Row>
             </div>
         );
     }
--- src/Components/Services.js
+++ src/Components/Services.js
@@ -12,7 +12,7 @@ function Services(){
             <div className="services-container">
                 <h1>Services</h1>
                 <div style={{ margin: '5%'}} className="wrapper">
-                    <Row style={{paddingLeft: '10%'}}>
+                    <Row style={{paddingLeft: '1%'}}>
                         <Col>
                             <h3 style={{textDecoration: 'underline'}}>What Services do I Provide?</h3> 
                         </Col>
@@ -27,15 +27,14 @@ function Services(){
     } else{
         return(
             <div className="phone-services-container">
-                <h1 id="services">Services</h1>
                 <div className="phone-wrapper">
-                    <h3 id="phone-title" 
-                    style={{textDecoration: 'underline', marginLeft: '50px', width: '500px', 
-                    marginBottom: '20px'
+                    <h2 id="phone-title" 
+                    style={{textDecoration: 'underline', marginLeft: '25px', width: '440px', 
+                    marginBottom: '20px', marginTop: '35px'
                     }}>
                         What Services do I Provide?
-                    </h3> 
-                    <h5 style={{width: '480px', marginBottom: '20px', marginLeft: '10px'}}>
+                    </h2> 
+                    <h5 style={{width: '480px', marginBottom: '40px', marginLeft: '10px'}}>
                     I can Provide a Full-Stack Website using ReactJS, ExpressJS and MongoDB.
                     Anything you would need I should be able to provide.
                     I do Freelancing only so I don't do any Full-Time positions.</h5>
