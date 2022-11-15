244d1e5a463a903f2b1d6c1f9a9650249f7ae7cc
lMwenda <Lukemwen@hotmail.com>
2022-11-15 22:31:16 +0000

    :hammer: Fixed a few build error

--- components/Layout/Projects.js
+++ components/Layout/Projects.js
@@ -3,14 +3,12 @@ import axios from "axios";
 import Card from "../Card";
 import data from "../data/projects.json"
 
-const BASE_URL = "http://localhost:3000/api"
-
 const Projects = () => {
     return(
         <div className="grid grid-rows-3 gap-4 justify-center items-center md:grid-flow-col">
             {
-                data.projects.map(project => (
-                    <Card project={project} />
+                data.projects.map((key, project) => (
+                    <Card key={key} project={project} />
                 ))
             }
         </div>
